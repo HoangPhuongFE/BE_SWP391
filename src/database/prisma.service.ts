653278405ed5d8a984b531120ef8f77129f60f4c
Nguyen Phuong Hoang <hoang093898xxx@gmail.com>
2025-05-05 19:38:16 +0700

    fix : DATABASE_URL

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -7,7 +7,7 @@ export class PrismaService extends PrismaClient
   async onModuleInit() {
     await this.$connect();
   }
-
+  // Cách này sẽ tự động ngắt kết nối khi ứng dụng tắt
   async onModuleDestroy() {
     await this.$disconnect();
   }
